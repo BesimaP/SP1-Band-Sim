c8bbe674fabd45729b3eaa92eb8489223f590f03
BesimaP <bepl1000@stud.ek.dk>
2026-01-30 15:38:35 +0100

    Ilteration 1: corrections

--- src/SP1_Band_Sim.java
+++ src/SP1_Band_Sim.java
@@ -10,7 +10,7 @@ void main() {
     int experiencePoints = 3200;
     double money = 4750.50;
     boolean isActive = true;
-    String[] genre = {"Rock","Electronic","HipHop","Pop"};
+    String genre = "Rock";
     String[] songs = {"Midnight moon", "Imagine", "White Christmas", "Broken Heart", "Nightmare"};
 
     System.out.println("=== BAND PROFILE ===");
@@ -21,6 +21,54 @@ void main() {
     System.out.println("ExperiencePoints: " + experiencePoints);
     System.out.println("Money: " + money);
     System.out.println("Active: " + isActive);
-    System.out.println("Genre: " + Arrays.toString(genre));
+    System.out.println("Genre: " + genre);
     System.out.println("Songs: " +  songs[0] +", "+ songs[1] + ", "+songs[2] +", "+ songs[3] +" and "+ songs[4]);
+
+     System.out.println(          );
+
+    //Mit band
+    String name = "Chaos Harmony";
+    int fansAmount = 7500;
+    int maximalFans = 13000;
+    int famousLevel = 1;
+    int expertisePoints = 3000;
+    double cash = 3755.50;
+    boolean isEnabled = true;
+    String musicStyle = "Pop";
+    String[] musicSongs = {"My heart","Christmas","Dreams"};
+
+    System.out.println("=== BAND PROFILE ===");
+    System.out.println("Name: " + name);
+    System.out.println("Fans: " + fansAmount);
+    System.out.println("Maximal fans: " + maximalFans);
+    System.out.println("Fame level: " + famousLevel);
+    System.out.println("Experience Points: " + expertisePoints);
+    System.out.println("Money "+ cash);
+    System.out.println("Active: "+isEnabled);
+    System.out.println("Genre: "+ musicStyle);
+    System.out.println("Songs: " + musicSongs[0] +", "+ musicSongs[1] +" and "+ musicSongs[2]);
+
+    System.out.println(          );
+
+    //Rival-band
+    String band = "The Rivals";
+    int audience = 6500;
+    int maxAudience = 12000;
+    int famousStatus = 2;
+    int skillScore = 2500;
+    double cashBalance = 355.50;
+    boolean isOperational = true;
+    String style = "Hiphop";
+    String[] musicHits = {"My heart","Christmas","Dreams"};
+
+    System.out.println("=== BAND PROFILE ===");
+    System.out.println("Name: " + band);
+    System.out.println("Fans: " + audience);
+    System.out.println("Max fans: " + maxAudience);
+    System.out.println("Fame level: " + famousStatus);
+    System.out.println("Experience points: " + skillScore);
+    System.out.println("Money: " + cashBalance);
+    System.out.println("Active: " + isOperational);
+    System.out.println("Genre:. " + style);
+    System.out.println("Songs: " + musicHits[0] + ", " + musicHits[1] + " and " + musicHits[2]);
 }
